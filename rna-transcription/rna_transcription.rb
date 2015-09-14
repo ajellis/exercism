e6a958c595bb7a58411f3ea37c22409ad73ae15e
Anthony Ellis <ajellis1989@gmail.com>
2015-09-14 18:24:48 -0400

    Adds passes all tests!

---
+++ rna-transcription/rna_transcription.rb
@@ -0,0 +1,45 @@
+class RnaTranscription
+end
+
+class Complement
+  def self.of_dna(type)
+    letter_array = []
+    type.each_char do |letter|
+      if letter == "U"
+        raise(ArgumentError)
+      elsif letter == "C"
+        letter_array << "G"
+      elsif letter == "G"
+        letter_array << "C"
+      elsif letter == "T"
+        letter_array << "A"
+      elsif letter == "A"
+        letter_array << "U"
+      elsif raise(ArgumentError)
+      end
+    end
+    letter_array.join
+  end
+
+  def self.of_rna(type)
+    letter_array = []
+    type.each_char do |letter|
+      if letter == "T"
+        raise(ArgumentError)
+      elsif letter == "C"
+        letter_array << "G"
+      elsif letter == "G"
+        letter_array << "C"
+      elsif letter == "U"
+        letter_array << "A"
+      elsif letter == "A"
+        letter_array << "T"
+      elsif raise(ArgumentError)
+      end
+    end
+      letter_array.join
+    end
+
+VERSION = 2
+
+end
