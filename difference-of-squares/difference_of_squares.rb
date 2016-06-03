369bedaef43bd1b1edbcc68be1101a898d5706c4
ajellisgit <ajellis1989@gmail.com>
2016-06-03 12:25:59 -0400

    All test pass

---
+++ difference-of-squares/difference_of_squares.rb
@@ -0,0 +1,41 @@
+class Squares
+def initialize(number)
+  @number = number
+  square_of_sum = 0.to_i
+  sum_of_squares = 0.to_i
+end
+
+  def square_of_sum
+    array = []
+    sum = 0
+    (1..@number).each do |n|
+      array << n
+    end
+    # puts "Your array is #{array}"
+    array.each do |j|
+      sum = sum + j
+    end
+    squared_sum = sum**2
+    square_of_sum = squared_sum
+  end
+
+  def sum_of_squares
+    array = []
+    sum = 0
+    (1..@number).each do |n|
+      squared_number = n**2
+      array << squared_number
+    end
+    array.each do |j|
+      sum = sum + j
+    end
+    sum_of_squares = sum
+    return sum_of_squares
+
+  end
+
+  def difference
+    square_of_sum - sum_of_squares
+  end
+
+end
