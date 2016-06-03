5367ecbfde87e9b6307c2c42a1af50850d2c882b
ajellisgit <ajellis1989@gmail.com>
2016-06-03 09:17:11 -0400

    First 2 tests pass

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,6 +1,10 @@
 class Raindrops
 
   def self.convert(number)
+    if number % 3 == 0
+      return "Pling"
+    else
     number.to_s
+    end
   end
 end
--- raindrops/raindrops_test.rb
+++ raindrops/raindrops_test.rb
@@ -13,7 +13,6 @@ class RaindropsTest < Minitest::Test
   end
 
   def test_3
-    skip
     assert_equal 'Pling', Raindrops.convert(3)
   end
 
