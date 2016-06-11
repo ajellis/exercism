2f81592e19bc594ccf8968db257978282c7e62eb
ajellisgit <ajellis1989@gmail.com>
2016-06-11 01:34:59 -0400

    raindrops completed

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,14 +1,20 @@
 class Raindrops
+  WORDS = {3 => "Pling", 5 => "Plang", 7 => "Plong"}
 
   def self.convert(number)
-    if number % 3 == 0
-      return "Pling"
-    elsif number % 5 == 0
-      return "Plang"
-    elsif number % 7 == 0
-      return "Plong"
-    else
-    number.to_s
+    factors = divisors_of(number)
+    answer = words(factors)
+    answer = number.to_s if answer.empty?
+    answer
+  end
+
+  def words(factors)
+    factors.inject('') do |answer, factor|
+      answer += (WORDS[factor]).to_s
     end
   end
+
+  def self.divisors_of(num)
+   (1..num).select { |n| num % n == 0}
+ end
 end
