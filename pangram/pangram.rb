0fe59db122157aba9b457e6796b3b44c28f7cc76
ajellisgit <ajellis1989@gmail.com>
2016-06-12 00:17:12 -0400

    Pangram finished

--- pangram/pangram.rb
+++ pangram/pangram.rb
@@ -1,7 +1,20 @@
 class Pangram
 
+  LETTERS = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+    "t", "u", "v", "w", "x", "y", "z"]
+
   def self.is_pangram?(str)
-    if str.include(str.match[abcdefghijklmnopqrstuvwxyz])
+    arr = str_array(str)
+    LETTERS.each do |l|
+      unless arr.include? l
+        return false
+      end
     end
   end
+
+  def self.str_array(str)
+    lower_string = str.downcase
+    arr = lower_string.split('')
+  end
+
 end
