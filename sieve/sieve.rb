ff872930d855735e3f3af26864bebf7dad26de5e
ajellisgit <ajellis1989@gmail.com>
2016-06-14 22:06:09 -0400

    still working on sieve

--- sieve/sieve.rb
+++ sieve/sieve.rb
@@ -6,27 +6,21 @@ class Sieve
 
   end
 
+
   def primes
-    (2..@number).each do |f|
-      if @number == 2
-        @primes << @number
-      elsif @number % f == 0
-        break
-      else
-        @primes << @number
+    (2..@number).each do |i|
+      (2...i).each do |j|
+        if j % i = 0
+          break
+        else
+        end
+          @primes << i
       end
     end
     @primes
   end
 
-  def blah
-    (2..@number).each do |i|
-      if @number == 2
-        @primes << @number
-      else (2...i).each do |j|
-        # if 
-  end
 
 end
 
-# I want to take a number (@number) and find each prime up to it. So I want to take each number before it (i), and divide it by each other number before it (j). if there is no remainder
+# I want to take a number (@number) and find each prime up to it. So I want to take each number before it (i), and divide it by each other number before it (j). if there is no remainder after that process is finished, I want to add i to primes
